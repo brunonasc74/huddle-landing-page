546b7714cd6503680197e8727fa21e31528faf71
brunonasc74 <bruno.hpn74@gmail.com>
2022-09-10 18:35:11 -0300

    Container and Header components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+import theme from './components/styles/theme';
+import GlobalStyles from './components/styles/GlobalStyles';
+import { Container } from './components/styles/Container.styled';
+import Header from './components/Header';
+
 function App() {
 	return (
-		<div className='App'>
-			<h1>Hello</h1>
-		</div>
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<>
+				<Header />
+				<Container>
+					<h1>Hello</h1>
+				</Container>
+			</>
+		</ThemeProvider>
 	);
 }
 
